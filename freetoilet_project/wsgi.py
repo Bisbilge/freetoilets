4fbde22061b195a08fc548608d5e1363a94d1517
bisbilge <bisbilge@gmail.com>
2026-02-21 18:09:49 +0000

    fix: wrapped wsgi with whitenoise to serve static files on vercel

--- freetoilet_project/wsgi.py
+++ freetoilet_project/wsgi.py
@@ -1,11 +1,15 @@
-# freetoilet_project/wsgi.py
-
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise # Bu satır kritik
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freetoilet_project.settings')
 
 application = get_wsgi_application()
 
-# Vercel'in beklentisi olan şu satırı ekliyoruz:
+# Statik dosyaların toplandığı klasörü WhiteNoise'a tanıtıyoruz
+# BASE_DIR'deki 'staticfiles' klasörüne bakmasını söylüyoruz
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+application = WhiteNoise(application, root=os.path.join(base_dir, 'staticfiles'))
+
+# Vercel'in çalıştırdığı değişken
 app = application
