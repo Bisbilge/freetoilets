2594b07739751fd2b742ca2c20fefbb7793d4b73
bisbilge <bisbilge@gmail.com>
2026-02-24 17:24:01 +0000

    fix: removed invalid views import from main urls.py and fixed routing

--- freetoilet_project/urls.py
+++ freetoilet_project/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Gelen trafiği 'toilets' uygulamasının urls.py dosyasına aktar
     path('', include('toilets.urls')), 
 ]
--- toilets/urls.py
+++ toilets/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/toilets/', views.toilet_data, name='toilet_data'),
-    path('report/', views.report_toilet, name='report_toilet'), # Yeni tuvalet ekleme
-    path('report-issue/', views.report_issue, name='report_issue'), # Haritadaki tuvaleti şikayet etme
+    path('report/', views.report_toilet, name='report_toilet'),
+    path('report-issue/', views.report_issue, name='report_issue'),
 ]
